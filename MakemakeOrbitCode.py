9862ebe6de4922278f64dac6c816704f7f711ba5
aye-kuma <113369939+aye-kuma@users.noreply.github.com>
2023-03-08 08:06:42 -0500

    Update MakemakeOrbitCode.py

--- MakemakeOrbitCode.py
+++ MakemakeOrbitCode.py
@@ -1,7 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-pathMakemake = "/Users/theo/Downloads/MakemakeVector.txt"
+def AreaT(a,b,theta1, theta2):
+    area = 0.5 * a * b * np.sin((theta2- theta1) * np.pi/180)
+    return area
+    
+# Was comparing the two to see if I needed to know side C in order to get the area 
+
+def AreaTri(a,b,theta1, theta2):
+    c = np.sqrt(a**2 + b**2 - 2*a*b*np.cos((theta2 - theta1) * np.pi/180))
+    s = 0.5 * (a + b + c)
+    area = np.sqrt(s* (s-a) * (s-b) * (s-c))
+    return area
+
+A1 = 45.4059866936633
+A2 = 41.0339992084371
+
+B1 = 38.2108754299483
+B2 = 40.0084524609087
+
+C1 = 40.0372279443707
+C2 = 38.4302797996442
+
+D1 = 51.7185730369357
+D2 = 52.6509625044766
+
+thetaA1 = 260.5437
+thetaA2 = 302.4433
+
+thetaB1 = 0.0256
+thetaB2 = 47.6182
+
+thetaC1 = 311.4853
+thetaC2 = 343.0647
+
+thetaD1 = 151.3121
+thetaD2 = 170.9338
+
+print(AreaT(A1,A2,thetaA1,thetaA2))
+print(AreaTri(A1,A2,thetaA1,thetaA2))
+
+print(AreaT(C1, C2, thetaC1, thetaC2))
+print(AreaTri(C1, C2, thetaC1, thetaC2))
+
+print(AreaT(B1, B2, thetaB1, thetaB2))
+print(AreaTri(B1, B2, thetaB1, thetaB2))
+
+print(AreaT(D1, D2, thetaD1, thetaD2))
+print(AreaTri(D1, D2, thetaD1, thetaD2))
+
+
+"""pathMakemake = "/Users/theo/Downloads/MakemakeVector.txt"
 dataMakemake = np.genfromtxt(pathMakemake, skip_header=50, skip_footer=48, delimiter=',', names=['JDTDB', 'Calendar Date', 'X','Y','Z'])
 
 pathEarth = "/Users/theo/Downloads/EarthSolOrbit.txt"
@@ -15,6 +64,4 @@ plt.xlabel('AU')
 plt.ylabel('AU')
 plt.legend()
 plt.grid()
-plt.show()
-
-
+plt.show()"""
